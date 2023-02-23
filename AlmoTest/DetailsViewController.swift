7b01e2c36d2a0a1afbcbbff8ae4af71b2b0d33db
Lama Darawsheh <lama.ndarawsheh@gmail.com>
2023-02-23 17:52:49 +0200

    test commit

--- AlmoTest/DetailsViewController.swift
+++ AlmoTest/DetailsViewController.swift
@@ -23,6 +23,7 @@ class DetailsViewController: UIViewController {
     
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .white
